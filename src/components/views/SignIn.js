afd34ba76b20b343257cd40acd338d7dd3aa9e5b
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-11-05 18:42:10 +0100

    styled elements mover to forFormik.js

--- src/components/views/SignIn.js
+++ src/components/views/SignIn.js
@@ -8,32 +8,7 @@ import * as Yup from 'yup';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 import { StyledH1 } from 'components/H1/H1';
-
-const StyledForm = styled(Form)`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  display: flex;
-  flex-direction: column;
-  width: 25%;
-  color: white;
-  ${({ theme }) => theme.media.phone} {
-    width: 90%;
-  }
-  ${({ theme }) => theme.media.landscape} {
-    width: 70%;
-  }
-`;
-
-const ChangedStyledButton = styled(StyledButton)`
-  align-self: flex-end;
-  margin-top: 2rem;
-  color: ${({ theme }) => theme.colors.dark};
-  ${({ theme }) => theme.media.landscape} {
-    margin-top: 1rem;
-  }
-`;
+import { StyledForm, ChangedStyledButton } from 'components/atoms/forFormik';
 
 const SignIn = () => {
   return (
