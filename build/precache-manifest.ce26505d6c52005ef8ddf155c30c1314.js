abe287e707d40bafc43ae377cc02be4d8e3a8e3f
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-11-12 17:51:57 +0100

    deployed

---
+++ build/precache-manifest.ce26505d6c52005ef8ddf155c30c1314.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990f025a68b4f16e269275a4ce45169b",
+    "url": "/notes.github.io/index.html"
+  },
+  {
+    "revision": "ee49fdcbf9bf9e425815",
+    "url": "/notes.github.io/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1f9cabcb96b0df224ad0",
+    "url": "/notes.github.io/static/css/main.711bbcfb.chunk.css"
+  },
+  {
+    "revision": "ee49fdcbf9bf9e425815",
+    "url": "/notes.github.io/static/js/2.5839447c.chunk.js"
+  },
+  {
+    "revision": "f1c9fd081828b9f5316001f1a89c2e1c",
+    "url": "/notes.github.io/static/js/2.5839447c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9cabcb96b0df224ad0",
+    "url": "/notes.github.io/static/js/main.009b27c4.chunk.js"
+  },
+  {
+    "revision": "d343de6fe465f9afdf51",
+    "url": "/notes.github.io/static/js/runtime-main.c0f6e797.js"
+  },
+  {
+    "revision": "4a3642db5f1a74e3eefd3e28bfcdcf15",
+    "url": "/notes.github.io/static/media/logout.4a3642db.svg"
+  }
+]);
