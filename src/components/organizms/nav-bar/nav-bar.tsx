a2f4e634018c1c8da863c29b9744fea2231c1e46
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2024-08-02 22:32:47 -0400

    chore: Update favicon file extension to PNG
    feat: Add logo to login and navigation bar
    
    chore: Refactor navbar logo to use Image component

--- src/components/organizms/nav-bar/nav-bar.tsx
+++ src/components/organizms/nav-bar/nav-bar.tsx
@@ -1,11 +1,12 @@
 import { NavBarButtonWrapper, NavBarUser } from '@notes/components';
 import classes from './styles.module.css';
-import { Box } from '@mantine/core';
+import { Box, Image } from '@mantine/core';
+import logo from '../../../assets/logo.svg';
 
 export const NavBar = () => {
   return (
     <Box className={classes.wrapper}>
-      <Box className={classes.logo}>XXX_LOGO_XXX</Box>
+      <Box className={classes.logo}><Image w="200px" src={logo} /></Box>
       <Box className={classes.bar}>
         <NavBarButtonWrapper />
         <NavBarUser />
