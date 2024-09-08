5df47090993633bba4f49720e77e5633f8f6e58a
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2024-09-07 22:54:21 -0400

    Refactor resetPassword function signature

--- src/hooks/manage-notes/use-update-note.ts
+++ src/hooks/manage-notes/use-update-note.ts
@@ -24,3 +24,44 @@ export const useUpdateNote = () => {
 };
 
 
+// return useMutation({
+//   mutationFn: () => checkoutBook(book.id),
+//   onMutate: async (element????) => {
+//     await queryClient.cancelQueries({
+//       queryKey: ["books"],
+//     });
+ 
+//     const snapshot = {
+//       myBooks: queryClient.getQueryData(["books", "my-books"]),
+//       bookDetail: queryClient.getQueryData(["books", "detail", book.id]),
+//     };
+
+//     queryClient.setQueryData(["books", "my-books"], (previousBooks) =>
+//       previousBooks ? [...previousBooks, book] : undefined
+//     );
+
+//     queryClient.setQueryData(["books", "detail", book.id], (previousBook) =>
+//       previousBook
+//         ? {
+//           ...previousBook,
+//           isCheckedOutByUser: true,
+//           availableCopies: previousBook.availableCopies - 1,
+//         }
+//         : undefined
+//     );
+//     return () => {
+//       queryClient.setQueryData(["books", "my-books"], snapshot.myBooks);
+//       queryClient.setQueryData(
+//         ["books", "detail", book.id],
+//         snapshot.bookDetail
+//       );
+//     };
+//   },
+
+//   onError: (err, _var, rollback) => {
+//     rollback?.();
+//   },
+//   onSettled: () => {
+//     return queryClient.invalidateQueries({ queryKey: ["books"] });
+//   },
+// });
