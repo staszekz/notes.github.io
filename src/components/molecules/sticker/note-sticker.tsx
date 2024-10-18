e49643562c61b2951e4ec526132ec4a7e591829d
staszekZ <staszek.zajaczkowski@gmail.com>
2024-10-18 00:12:37 -0400

    Refactor NoteSticker component to accept data as props; enhance styles and layout for improved presentation

--- src/components/molecules/sticker/note-sticker.tsx
+++ src/components/molecules/sticker/note-sticker.tsx
@@ -1,17 +1,12 @@
 import { Box, Text } from '@mantine/core';
-import { getNotesQueryOptions } from '@notes/rq';
-import { useSuspenseQuery } from '@tanstack/react-query';
 import classes from './sticker.module.css';
+import { NoteWithId } from '@notes/types';
 
-export function NoteSticker() {
-  const { data: notes } = useSuspenseQuery(getNotesQueryOptions());
-
-  return notes.map(note => {
-    return (
-      <Box className={classes.stickerBox}>
-        <Text>Paper is the most basic ui component</Text>
-        <Text>Use it to create cards, dropdowns, modals and other components that require background with shadow</Text>
-      </Box>
-    );
-  });
+export function NoteSticker({ data }: { data: NoteWithId }) {
+  return (
+    <Box component="li" className={classes.stickerBox}>
+      <Text size="xl">{data.title}</Text>
+      <Text>{data.content}</Text>
+    </Box>
+  );
 }
--- src/components/organizms/stickers/stickers.tsx
+++ src/components/organizms/stickers/stickers.tsx
@@ -1,5 +1,18 @@
-export const Stickers = ({ data, Component }) => {
-  return data.map(note => {
-    return <Component data={note} />;
-  });
-};
+import { SimpleGrid } from '@mantine/core';
+import classes from './stickers.module.css';
+
+export function Stickers<T extends { id: number }>({
+  data,
+  Component
+}: {
+  data: T[];
+  Component: React.FC<{ data: T }>;
+}) {
+  return (
+    <SimpleGrid spacing="xl" component={'ul'} cols={4} p={24} className={classes.stickerWrapper}>
+      {data.map(element => {
+        return <Component data={element} key={element.id} />;
+      })}
+    </SimpleGrid>
+  );
+}
